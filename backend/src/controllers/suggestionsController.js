9aab32f6cd0e586a0757e0c1b32278678bae4fe9
celestemarieb <34022835+celestemarieb@users.noreply.github.com>
2024-12-15 23:06:24 +1100

    read database

--- backend/src/controllers/suggestionsController.js
+++ backend/src/controllers/suggestionsController.js
@@ -1,14 +1,21 @@
+import { Request, Response } from 'express';
+import {ObjectID, Db} from 'mongodb'
+//import {Suggestion} from '../models/suggestions.model'
+
 // create
 
-export const createSuggestion() => async() => {
-    try {
-        const { name } req.body;
-
-        if {!name || typeof name !== "string"}
-    }
-}
-
 // read
+
+export const getAllSuggestions = (db: Db) => async(_: Request, res: Response) => {
+    try {
+        const suggestions = await db.collection("suggestions").find({}).toArray()
+        console.log("Suggestions read successfully")
+        res.json(suggestions)
+    } catch (err) {
+        console.log('Error reading suggestions',err)
+        return res.status(500).send('Error reading suggestions')
+    }
+};
 
 // update
 
